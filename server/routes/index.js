93387762728505f5048d46ca2ae1ef902e004985
Jeff Lynch <jlynch3@uw.edu>
2021-10-12 12:17:52 -0700

    first commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,23 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+const { 
+  ApolloServerPluginLandingPageProductionDefault,
+  ApolloServerPluginLandingPageLocalDefault,
+} = require('apollo-server-core');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [
+
+    process.env.NODE_ENV === 'production' ?
+      ApolloServerPluginLandingPageProductionDefault({ footer: false }) :
+      ApolloServerPluginLandingPageLocalDefault({ footer: false })
+  ]
+});
+
+
 // serve up react front-end in production
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
